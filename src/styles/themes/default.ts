84694acabf2de33512ecd8a2d62034eb8d08117d
Edeilton Oliveira <edeiltonso@gmail.com>
2023-03-29 01:12:21 -0300

    first commit

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,7 @@
+export const defaultTheme = {
+    primary: 'rgb(168, 0, 246)',
+    secondary: 'rgb(255, 173, 74)',
+    success: 'rgb(69, 185, 69)',
+    danger: 'rgb(255, 115, 115)',
+    white: 'rgb(255, 255, 255)'
+}
